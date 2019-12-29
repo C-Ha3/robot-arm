32bdda4574dad3bd4e4807754d71e7ac09033e8c
Charles Ha <charles.ha@mechasys.ca>
2019-12-29 18:23:24 -0500

    Changed code structure and controlling servo works

--- joystick-control.cpp
+++ joystick-control.cpp
@@ -1,28 +1,11 @@
-#include <Servo.h>
 #include "joystick-control.hpp"
 
 /******************************************************************************/
 //Constructor and Destructore
-JoystickControl::JoystickControl(){
-
-  servo1.attach(SERVO1_PIN);
-  servo2.attach(SERVO2_PIN);
-
-}
-
-void JoystickControl::servo1move1() {
-    digitalWrite(SERVO1_PIN, HIGH);
-    delayMicroseconds(1450); // Duration of the pusle in microseconds
-    digitalWrite(SERVO1_PIN, LOW);
-    delayMicroseconds(18550); // 20ms - duration of the pusle
-
-}
-void JoystickControl::servo1move2() {
-    digitalWrite(SERVO1_PIN, HIGH);
-    delayMicroseconds(2300); // Duration of the pusle in microseconds
-    digitalWrite(SERVO1_PIN, LOW);
-    delayMicroseconds(18550); // 20ms - duration of the pusle
-
+JoystickControl::JoystickControl(Servo *servo1, Servo *servo2)
+{
+  this->servo1 = servo1;
+  this->servo2 = servo2;
 }
 
 JoystickControl::~JoystickControl();
--- joystick-control.hpp
+++ joystick-control.hpp
@@ -4,23 +4,14 @@
 #define SERVO1_PIN 9
 #define SERVO2_PIN 5
 
-class JoystickControl {
-    private:
-       
-    public:
-        JoystickControl();
+class JoystickControl
+{
+private:
 
-        Servo servo1;
-        Servo servo2;
+public:
+    Servo *servo1;
+    Servo *servo2;
+    JoystickControl(Servo* servo1, Servo* servo2);
 
-        void servo1To(int angle1) {servo1.write(angle1);}
-        void servo2To(int angle1) {servo2.write(angle1);}
-        void servo1move1();
-        void servo1move2();
-
-        int readServo1() {servo1.read();}
-        int readServo2() {servo2.read();}
-
-        ~JoystickControl();
-
+    ~JoystickControl();
 };
